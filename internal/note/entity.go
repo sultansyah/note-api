a90c14734acdb7478f2a2d0cb3ea4ec743f59748
Muhammad Sultansyah <muhammadsultansyah14@gmail.com>
2024-12-21 01:29:53 +0700

    feat: add user in entity note

--- internal/note/entity.go
+++ internal/note/entity.go
@@ -1,14 +1,20 @@
 package note
 
-import "time"
+import (
+	"time"
+
+	"github.com/sultansyah/note-api/internal/user"
+)
 
 type Note struct {
 	Id        int       `json:"id"`
+	UserId    int       `json:"user_id"`
 	Note      int       `json:"note"`
 	Status    string    `json:"status"`
 	Priority  string    `json:"priority"`
 	Category  string    `json:"category"`
 	Tags      string    `json:"tags"`
+	User      user.User `json:"user"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
